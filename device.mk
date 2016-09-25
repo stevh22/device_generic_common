620163cdf256a6ea2189500d38eddd2fc59ecc5a
CnG <true.weniz@gmail.com>
2016-09-25 18:23:09 +0100

    enable nativebridge from the first boot

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     ro.sf.lcd_density=160 \
+    persist.sys.nativebridge=1 \
 	
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
     ro.arch=x86 \
