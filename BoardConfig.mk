38685b10cad1ca49bf00b0d56ed364973eace53b
CnG <true.weniz@gmail.com>
2016-09-25 18:14:28 +0100

    remove SuperSu, I'll use CM's SuperSu

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,9 +55,6 @@ BOARD_USES_DRM_HWCOMPOSER ?= true
 
 USE_CAMERA_STUB ?= false
 
-SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
-
 # This enables the wpa wireless driver
 BOARD_WPA_SUPPLICANT_DRIVER ?= NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB ?= private_lib_driver_cmd
