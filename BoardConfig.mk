7a7e315cb905ff127187fb10464b19bc810cbfff
Jon West <electrikjesus@gmail.com>
2019-01-25 20:01:42 -0500

    Update Bliss version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,8 +117,8 @@ WITH_BLISS_CHARGER := false
 TARGET_PC_BUILD := true
 
 # Bliss OS specific 
-VER := 11.0
+VER := 11.4
 RELEASE_OS_TITLE := Bliss-OS 
 BUILD_NAME_VARIANT := bliss-x86 
 INSTALL_PREFIX := bliss-x86-$(VER) 
-PREV_VERS := bliss-x86-6.4 bliss-x86-7.1.2 bliss-x86-8.0 bliss-x86-10.0 bliss-x86-10.1
+PREV_VERS := bliss-x86-6.4 bliss-x86-7.1.2 bliss-x86-8.0 bliss-x86-10.0 bliss-x86-10.1 bliss-x86-11.0
