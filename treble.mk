23f9192ca107893dbaa59663eae06df203299378
Jon West <electrikjesus@gmail.com>
2019-05-01 21:48:26 -0400

    remove redundant stuff

--- treble.mk
+++ treble.mk
@@ -74,10 +74,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
-
 # usb accessory
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
