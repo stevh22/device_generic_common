8f0edf129d723fa75f33b4b036abf5ce8ce03e9e
electrikjesus <electrikjesus@gmail.com>
2019-07-05 12:45:58 -0400

    Add i915drmfb to hwaccel list.

--- init.sh
+++ init.sh
@@ -187,7 +187,7 @@ function init_hal_gralloc()
 				set_property debug.drm.mode.force ${video:-1280x800}
 			fi
 			;;
-		0*inteldrmfb|0*radeondrmfb|0*nouveaufb|0*svgadrmfb|0*amdgpudrmfb)
+		0*i915drmfb|0*inteldrmfb|0*radeondrmfb|0*nouveaufb|0*svgadrmfb|0*amdgpudrmfb)
 			if [ "$HWACCEL" != "0" ]; then
 				set_property ro.hardware.hwcomposer ${HWC:-drm}
 				set_property ro.hardware.gralloc ${GRALLOC:-gbm}
